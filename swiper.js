6506d683dc18d077a4cca15eb1a247a8ac1cd218
BalandinaM <marina.balandina.89@mail.ru>
2024-09-15 01:02:40 +0500

    стиль скроллбара

--- swiper.js
+++ swiper.js
@@ -2,17 +2,24 @@ const swiper = new Swiper('.swiper', {
   // Optional parameters
   direction: 'horizontal',
   loop: true,
+  //nested: true,
 
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-  },
+  // // If we need pagination
+  // pagination: {
+  //   el: '.swiper-pagination',
+  // },
+
+  // pagination: {
+  //   type: 'fraction',
+  // },
 
   // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
+  // navigation: {
+  //   nextEl: '.swiper-button-next',
+  //   prevEl: '.swiper-button-prev',
+  // },
+
+  //navigation: true,
 
   // And if we need scrollbar
   scrollbar: {
