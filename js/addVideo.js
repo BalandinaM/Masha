adf3384942425c53a4f52daeff994ca14d173b77
BalandinaM <marina.balandina.89@mail.ru>
2024-10-16 00:00:54 +0500

    подтянула постеры

--- js/addVideo.js
+++ js/addVideo.js
@@ -1,10 +1,11 @@
 import { dataBase } from "./dataVideo.js";
 
 class Video {
-  constructor(srcMp4, srcOgv, srcAvi, parent) {
+  constructor(srcMp4, srcOgv, srcAvi, poster, parent) {
     this.srcMp4 = srcMp4;
     this.srcOgv = srcOgv;
     this.srcAvi = srcAvi;
+    this.poster = poster;
     this.parent = document.querySelector(parent);;
   }
 
@@ -13,6 +14,7 @@ class Video {
     const elementTemplate = document.querySelector('#video-item').content.querySelector('.exsamplesOfWorks__video');
     const elem = elementTemplate.cloneNode(true);
 
+    elem.querySelector('.video').setAttribute('poster', this.poster);
     elem.querySelector('.video-item--mp4').setAttribute('src', this.srcMp4);
     elem.querySelector('.video-item--ogg').setAttribute('src', this.srcOgv);
     elem.querySelector('.video-item--avi').setAttribute('src', this.srcAvi);
@@ -22,6 +24,6 @@ class Video {
 }
 
 dataBase.forEach(elem => {
-  new Video(elem.mp4, elem.ogv,elem.avi, '.exsamplesOfWorks__wrapper').render();
+  new Video(elem.mp4, elem.ogv,elem.avi, elem.poster, '.exsamplesOfWorks__wrapper').render();
 });
 
